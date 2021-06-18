b83ce448988e21840c5d06ad1d9ba908d38acff3
headassbtw <cooldood909@gmail.com>
2021-06-18 15:45:06 -0700

    fixed me being a fucking idiot

--- IgnoreCinema/Plugin.cs
+++ IgnoreCinema/Plugin.cs
@@ -1,8 +1,4 @@
 ﻿using IPA;
-using IPALogger = IPA.Logging.Logger;
-using HarmonyLib;
-using System.Reflection;
-using UnityEngine.UI;
 
 namespace IgnoreCinema
 {
@@ -10,38 +6,12 @@ namespace IgnoreCinema
     public class Plugin
     {
         internal static Plugin Instance { get; private set; }
-        internal static IPALogger Log { get; private set; }
-        internal static Harmony harmony { get; private set; }
 
         [Init]
-        public void Init(IPALogger logger)
+        public void Init()
         {
             Instance = this;
-            Log = logger;
-            harmony = new Harmony("com.headassbtw.ignorecinema");
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            SongCore.Collections.RegisterCapability("Cinema");
         }
     }
-    [HarmonyPatch(typeof(StandardLevelDetailView))]
-    [HarmonyPatch("RefreshContent", MethodType.Normal)]
-    [HarmonyAfter("com.kyle1413.BeatSaber.SongCore")]
-    internal class RequirementPatch
-    {
-        internal static void Postfix(StandardLevelDetailView __instance, ref IDifficultyBeatmap ____selectedDifficultyBeatmap, ref Button ____actionButton, ref Button ____practiceButton)
-        {
-            Plugin.Log.Notice("Fuckin Gamer");
-            var diffData = SongCore.Collections.RetrieveDifficultyData(____selectedDifficultyBeatmap);
-
-            for (int i = 0; i < diffData.additionalDifficultyData._requirements.Length; i++)
-            {
-                if (diffData.additionalDifficultyData._requirements[i].ToLower().Equals("cinema"))
-                {
-                    ____actionButton.interactable = true;
-                    ____practiceButton.interactable = true;
-                }
-            }
-        }
-
-    }
-
 }
